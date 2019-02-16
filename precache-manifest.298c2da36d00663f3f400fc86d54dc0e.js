09759cb42cc6ee6097469ae09aa259751dfda1d3
AlenaKharytonchyk <alena.kharytonchyk@outlook.com>
2019-02-16 16:21:46 +0300

    Updates

---
+++ precache-manifest.298c2da36d00663f3f400fc86d54dc0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa63da9d295c0bf5ff26a17210ec8b78",
+    "url": "/mentors-dashboard/static/media/rss.aa63da9d.png"
+  },
+  {
+    "revision": "0bae0569f0101747dba5",
+    "url": "/mentors-dashboard/static/js/runtime~main.0bae0569.js"
+  },
+  {
+    "revision": "8038a92acf5bf5fe421c",
+    "url": "/mentors-dashboard/static/js/main.8038a92a.chunk.js"
+  },
+  {
+    "revision": "47a38ede47fc8479b8ea",
+    "url": "/mentors-dashboard/static/js/1.47a38ede.chunk.js"
+  },
+  {
+    "revision": "8038a92acf5bf5fe421c",
+    "url": "/mentors-dashboard/static/css/main.655228ea.chunk.css"
+  },
+  {
+    "revision": "914798bd5c380ac53a56d1707ab69ed9",
+    "url": "/mentors-dashboard/index.html"
+  }
+];
